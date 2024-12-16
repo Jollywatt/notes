598547087ffac7cf8bf3be2eca24fbb99d26dac6
jollywatt <jo.alex.w@gmail.com>
2024-12-16 15:21:58 +0000

    add CC BY-NC-ND 4.0 licence

--- src/templates.jl
+++ src/templates.jl
@@ -5,7 +5,13 @@ ROOT = "/notes"
 
 permalink(url) = "$SITE$ROOT/"*url
 
-link(name, ext, label) = "• <a href=$(repr("$name.$ext"))>$label</a>"
+link(url, label) = "• <a href=$(repr(url))>$label</a>"
+
+copyright_url() = "https://creativecommons.org/licenses/by-nc-nd/4.0/"
+
+copyright_footer() = """
+	<p xmlns:cc="http://creativecommons.org/ns#" ><a rel="cc:attributionURL" href="https://github.com/Jollywatt/notes">This work</a> by <span property="cc:attributionName">Joseph Wilson</span> is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-ND 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nd.svg?ref=chooser-v1" alt=""></a></p>
+	"""
 
 base(content; title, head="") = """
 	<!DOCTYPE html>
@@ -27,22 +33,23 @@ headercontent(n) = """
 	<a href="$ROOT">Joseph’s notes</a> / <span style="font-family: monospace; font-size: initial;">$(n.name)</span>
 	$(
 		if :pdf in keys(n.files)
-			link(n.name, :pdf, "raw PDF")
+			link("$(n.name).pdf", "raw PDF")
 		else
 			""
 		end
 	)
 	$(
 		if :typ in keys(n.files)
-			link(n.name, :typ, "typst source")
+			link("$(n.name).typ", "typst source")
 		elseif :tex in keys(n.files)
-			link(n.name, :tex, "LaTeX source")
+			link("$(n.name).tex", "LaTeX source")
 		elseif :jl in keys(n.files)
-			link(n.name, :jl, "Julia source")
+			link("$(n.name).jl", "Julia source")
 		else
 			""
 		end
 	)
+	$(link(copyright_url(), "©"))
 """
 
 
@@ -126,6 +133,9 @@ toc(tree) = base("""
 		</p>
 
 		$(toc_item(tree))
+
+		$(copyright_footer())
+
 	</div>
 	"""; title = "Home")
 
