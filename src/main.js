c504fc4f5e7cd40c917927ec71faa007bd137d2e
jollywatt <jo.alex.w@gmail.com>
2024-12-30 00:40:53 +0000

    use own theme

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-import { Minimal, Project } from "zettelsite";
+import { Minimal, Project } from "zettelsite"
+import { build } from "./theme.tsx"
 
 const project = new Project({
-  srcdir: "notes/",
-  sitedir: "site/",
-  noteTypes: {
-    "markdown": ["md"],
-    "typst pdf": ["typ", "pdf"],
-    "latex pdf": ["tex", "pdf"],
-    "pluto notebook": ["jl", "html"],
-    "julia code": ["jl"],
-    "plain text": ["txt"],
-    "external link": ["url"],
-  },
-  builder: Minimal.build,
-});
+	srcdir: "notes/",
+	sitedir: "site/",
+	noteTypes: {
+		"markdown": ["md"],
+		"typst pdf": ["typ", "pdf"],
+		"latex pdf": ["tex", "pdf"],
+		"pluto notebook": ["jl", "html"],
+		"julia code": ["jl"],
+		"plain text": ["txt"],
+		"external link": ["url"],
+	},
+	builder: build,
+})
 
-const [command] = Deno.args;
+const [command] = Deno.args
 
-if (command === "build") project.build();
-else if (command === "serve") project.serve();
+if (command === "build") project.build()
+else if (command === "serve") {
+	project.build()
+	project.serve()
+}
---
+++ src/theme.tsx
@@ -0,0 +1,164 @@
+import { Note, NoteFolder, Project } from "zettelsite"
+import { CSS, render as renderMarkdown } from "@deno/gfm"
+import { copySync } from "@std/fs"
+import { join as pathJoin } from "@std/path"
+
+const base = ({ head, body }) => (
+	<html>
+		<head>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="stylesheet" href="./assets/style.css" />
+			<link rel="stylesheet" href="./assets/widgets.css" />
+			{head}
+			<title>Joseph's notes | $title</title>
+		</head>
+		<body>
+			{body}
+		</body>
+	</html>
+)
+
+const json = (obj) => <pre>{JSON.stringify(obj, null, 2)}</pre>
+
+// deno-fmt-ignore
+const copyrightFooter = `<p xmlns:cc="http://creativecommons.org/ns#"><a rel="cc:attributionURL" href="https://github.com/Jollywatt/notes">This work</a> by <span property="cc:attributionName">Joseph Wilson</span> is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-ND 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""/><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""/><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""/><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nd.svg?ref=chooser-v1" alt=""/></a></p>`
+const copyrightURL = "https://creativecommons.org/licenses/by-nc-nd/4.0/"
+
+const indexPage = (tree: NoteFolder) =>
+	base({
+		head: <title>Index</title>,
+		body: (
+			<>
+				<img id="background" src="./assets/background.png" />
+				<div id="toc" className="pad">
+					<h1>
+						<a href="/">Joseph</a>’s notes V. III
+					</h1>
+					<p>
+						Welcome to my Zettelkasten garden of notes. Here is where I put
+						scraps and notes from my research and coursework.
+					</p>
+
+					{toc(tree)}
+
+					<div dangerouslySetInnerHTML={{ __html: copyrightFooter }} />
+				</div>
+			</>
+		),
+	})
+
+const tocEntry = (note: Note) => (
+	<li>
+		<a className="notelink" href={`${note.name}`}>
+			[{note.name}]
+		</a>{" "}
+		<span style={{ fontSize: "80%" }}>{note.type}</span>
+	</li>
+)
+
+function toc(node: NoteFolder) {
+	const notenames = Object.keys(node.notes).sort()
+	const foldernames = Object.keys(node.folders).sort()
+	return (
+		<ul>
+			{notenames.map((name) => tocEntry(node.notes[name]))}
+			{foldernames.map((name) => (
+				<li>
+					{name}
+					{toc(node.folders[name])}
+				</li>
+			))}
+		</ul>
+	)
+}
+
+function headerContent(note: Note) {
+	let el = <>
+		<a href="/">Joseph’s notes</a> / {note.dir.map(a => `${a} / `)}
+		<span className="notelink">{note.name}</span>
+	</>
+	el = (
+		<>
+			{el} • <a href={copyrightURL}>&copy;</a>
+		</>
+	)
+	return el
+}
+
+const header = (note) => (
+	<div id="header">
+		{headerContent(note)}
+	</div>
+)
+
+const defaultRenderer = (note) =>
+	base({
+		head: <title>{note.name}</title>,
+		body: (
+			<main>
+				{header(note)}
+				No renderer is defined for note type <code>{note.type}</code>.
+				<pre>{JSON.stringify(note, null, 2)}</pre>
+			</main>
+		),
+	})
+
+const noteRenderers: { [noteType: string]: Function } = {}
+
+noteRenderers["markdown"] = async function (note) {
+	let md = await Deno.readTextFile(note.files.md)
+	md = md
+		.replace(/\(@([-\w]+)\)/g, (_, name) => `(${name}.html)`)
+		.replace(/@([-\w]+)/g, (handle, name) => `[${handle}](${name}.html)`)
+	const html = renderMarkdown(md)
+	return base({
+		head: (
+			<>
+				<title>{note.name}</title>
+				<style>{CSS}</style>
+			</>
+		),
+		body: (
+			<>
+				{header(note)}
+				<pre>{md}</pre>
+				<pre>{html}</pre>
+				<main
+					dangerouslySetInnerHTML={{ __html: html }}
+					className="markdown-body"
+				>
+				</main>
+			</>
+		),
+	})
+}
+
+noteRenderers["plain text"] = async function (note) {
+	const txt = await Deno.readTextFile(note.files.txt)
+	return base({
+		head: <title>{note.name}</title>,
+		body: (
+			<>
+				{header(note)}
+				<main>
+					<pre>{txt}</pre>
+				</main>
+			</>
+		),
+	})
+}
+
+export async function build(project: Project) {
+	const { notes, tree } = project.analyse()
+	project.renderPage("index.html", indexPage(tree))
+
+	copySync("src/assets", pathJoin(project.sitedir, "assets"))
+
+	for (const name in notes) {
+		const note: Note = notes[name]
+		const renderer = noteRenderers[note.type ?? "unknown"] ?? defaultRenderer
+		const html = await renderer(note)
+		project.renderPage(`${name}.html`, html)
+	}
+}
